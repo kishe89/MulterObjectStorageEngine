54af09e555c91b0d896c59aa747a409eed850f1c
kjw <kishe56@gmail.com>
2017-11-21 04:56:50 +0900

    var -> const modify

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,13 @@ const storageEngine = require('../util/ObjectStorageEngine');
 
 const upload = multer({
     storage:storageEngine({
-        destination: function (req, file, cb) {
+        destination:  (req, file, cb)=> {
             cb(null, 'profile')
         }
     })
 });
 
-router.post('/',upload.array('photos'), function(req, res, next) {
+router.post('/',upload.array('photos'), (req, res, next)=> {
   res.json(req.files);
 });
 
